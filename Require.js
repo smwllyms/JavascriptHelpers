41c95bab565ba160cf3b06b4714a11f9ad68e9f3
smwllyms <68836604+smwllyms@users.noreply.github.com>
2021-11-05 20:58:09 -0400

    Create Require.js

---
+++ Require.js
@@ -0,0 +1,28 @@
+// Waits for javascsript file from path $s to load and adds it to the page
+// Must be awaited for
+const require = async(s) => {
+    // Fetch the script
+    const response = await fetch(s);
+    if (!response.ok) 
+        return false;
+    // Await the blob
+    const blob = await response.blob(); 
+    const objectURL = URL.createObjectURL(blob);
+    // Create script element 
+    const sc = document.createElement("script");
+    sc.setAttribute("src", objectURL); 
+    sc.setAttribute("type", "text/javascript"); 
+    // Append it to the head to make it active
+    document.head.appendChild(sc);
+    // Create a promise we can use to wait until the script is loaded
+    let loaded = false;
+    let promiseResolve;
+    const loadedPromise = new Promise((resolve) => {
+        // We will call this externally
+        promiseResolve = resolve;
+    });
+    // When loaded resolve the promise and return to caller
+    sc.onload = function(){ loaded = true; promiseResolve();}
+    // Wait for script to load
+    await loadedPromise;
+}
