df6242833d8c7181b1b8bc898896895ec567f9f5
smwllyms <68836604+smwllyms@users.noreply.github.com>
2021-11-05 21:26:02 -0400

    Update Require.js
    Added css capabilities

--- Require.js
+++ Require.js
@@ -1,6 +1,6 @@
-// Waits for javascsript file from string path $path to load and add to page
-// Must be awaited upon
-const require = async(path) => {
+// Waits for javascsript/css file from string path $path to load and add to page
+// Must be awaited upon, type is either "css" or "js"
+const require = async(path, type) => {
     // Fetch the script
     const response = await fetch(path);
     if (!response.ok) 
@@ -8,16 +8,26 @@ const require = async(path) => {
     // Await the blob
     const blob = await response.blob(); 
     const objectURL = URL.createObjectURL(blob);
-    // Create script element 
-    const sc = document.createElement("script");
-    sc.setAttribute("src", objectURL); 
-    sc.setAttribute("type", "text/javascript"); 
+    let elem;
+    if (type == "js") {
+        // Create script element 
+        elem = document.createElement("script");
+        elem.setAttribute("src", objectURL); 
+        elem.setAttribute("type", "text/javascript"); 
+    }
+    else if (type == "css") {
+        // Create css element
+        elem = document.createElement("link");
+        elem.setAttribute("rel", "stylesheet");
+        elem.setAttribute("href", objectURL); 
+    }
+    else return false;
     // Append it to the head to make it active
-    document.head.appendChild(sc);
+    document.head.appendChild(elem);
     // Create a promise we can use to wait until the script is loaded
     const loadedPromise = new Promise((resolve) => {
         // When loaded resolve the promise and return to caller
-        sc.onload = resolve;
+        elem.onload = resolve;
     });
     // Wait for script to load
     await loadedPromise;
